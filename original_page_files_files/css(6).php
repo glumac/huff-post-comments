00bbab71bc57dd00d74bb1010bebeb49e5266038
test <mglumac@gmail.com>
2013-12-23 00:43:52 -0500

    formatted and stripped article page

---
+++ original_page_files_files/css(6).php
@@ -0,0 +1,101 @@
+/* From: wfe5-dtc : 27104 */
+
+.recommendation {
+    position: fixed;
+    right: -300px;
+    width: 300px;
+    background: #fff;
+    font: 12px Arial,Helvetica,sans-serif;
+    border-top: 2px solid #c00;
+    overflow: hidden;
+    display: none;
+    padding-bottom: 10px;
+    text-align: left;
+    border-bottom-left-radius: 10px;
+    -moz-border-radius-bottomleft: 10px;
+    -webkit-border-bottom-left-radius: 10px;
+    box-shadow: 5px 5px 5px rgba(0,0,0,.5);
+    -moz-box-shadow: -2px 2px 5px rgba(102,102,102,.5);
+    -webkit-box-shadow: -2px 2px 5px rgba(102,102,102,.5);
+}
+.weird-news .recommendation {
+    border-color: #ed7d00;
+}
+.crime .recommendation {
+    border-color: #513420;
+}
+.ie { 
+    border: 1px solid;
+    border-top: 2px solid;
+}
+.recommendationWrapper {
+    padding: 10px;
+    float: left;
+    width: 260px;
+}
+span.row { display: block }
+.recommendation a {
+    text-decoration: none;
+    color: #c00;
+    font-size: 11px;
+}
+span.recommendationCategory {
+    text-transform: uppercase;
+    font-size: 11px;
+    font-size: 10px;
+}
+span.recommendationTitle { margin: 5px 0px; }
+span.recommendationTitle a { font-size: 14px }
+.imgReplace { display: block }
+.imgReplace span { display: none }
+div.openWrapper {
+    width: 20px;
+    float: left;
+    display: none;
+}
+a.openRecommendation {
+    width: 13px;
+    height: 13px;
+    background: url(http://cdn.sailthru.com/assets/images/icons/open.gif) 0 0 no-repeat;
+    margin-top: 36px;
+    margin-left: 5px;
+}
+a.openRecommendation:hover { background: url(http://cdn.sailthru.com/assets/images/icons/open.gif) 0 -13px no-repeat }
+a.closeRecommendation {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    width: 13px;
+    height: 13px;
+    background: url(http://cdn.sailthru.com/assets/images/icons/close.gif) no-repeat;
+}
+.recommendationThumb {
+    float: left;
+    width: 112px;
+    height: 82px;
+    background: #ccc;
+}
+.recommendationThumb img {
+    width: 112px;
+    height: 82px;
+}
+.recommendationDetails {
+    float: right;
+    margin-left: 5px;
+    width: 135px;
+}
+.recommendationFooter {
+    font-size: 9px;
+    text-align: right;
+    clear: both;
+    padding-right: 5px;
+    color: #CDCDCD;
+}
+.recommendationFooter a {
+    font-size: 9px;
+    color: #CDCDCD;
+}
+
+
+
+/* From: wfe5-dtc : 27104 *//* c9f3f6b07bda2ab06bdea00b5f6d94c240a9aff3 */
