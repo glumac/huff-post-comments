28554456942479363a0fceeb691cac2caa33fde9
test <mglumac@gmail.com>
2013-12-28 17:16:36 -0500

    adding rails CRUD functionality for comment list (to be angular-ed)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,26 @@
 class CommentsController < ApplicationController
 
-	def index
+  def index
 		@comments = Comment.all
+		# @comment = Comment.new(params[:comment])
 	end
 
 	def show
 		@comment = Comment.find(params[:id])
-	end
 
-	def new
-		@comment = Comment.new
+  	respond_to do |format|
+       format.html # show.html.erb
+       format.json { render json: @comment } 
+  	end
+  end
+
+	def create
+		@comment = Comment.new(params[:comment])
 	end
 
 	def save_file
 	    audio = params[:audio]
-	    save_path = Rails.root.join("public/#{audio.original_filename}")
+	    save_path = Rails.root.join("public/audio/#{audio.original_filename}")
 	    audio.rewind
 	      # Open and write the file to file system.
 	      File.open(save_path, 'wb') do |f|
@@ -35,3 +41,22 @@ end
 
 
  
+
+ def show 
+
+   @student = Student.find_by_url(params[:id])
+
+   #BULLETS Randomizing /students/new.html.erb
+   if cookies[:bullets].nil?
+     @bullets = Bullet.all.shuffle.first(4)
+     cookies[:bullets] = @bullets.collect(&:id)
+   else
+     # simpler to use an 'in list' for only 4 id's
+     Bullet.where("id in (?)", cookies[:bullets])
+   end
+
+   respond_to do |format|
+     format.html # show.html.erb
+     format.json { render json: @student } 
+   end  
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   attr_accessible :clip, :name, :score
+  mount_uploader :Clip, ClipUploader
 end
+
