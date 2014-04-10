20d97a21cafe515640561095debd8cb244f1643c
glumac <mglumac@gmail.com>
2014-04-10 19:12:35 -0400

    code cleanup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,33 @@
 class CommentsController < ApplicationController
 
-  respond_to :html, :js
-
-  def index
-		@comments = Comment.all
-    @comment = Comment.new
-
-   # respond_to do |format|
-   #  format.html # index.html.erb
-   #  format.json { render json: @comments }
-   # end
+	respond_to :html, :js
+	
+	def index
+	@comments = Comment.all
+	@comment = Comment.new
 	end
 
 	def new
-  end
+	end
 
-  def create
-  end
+	def create
+	end
 
 	def save_file
-    audio = params[:audio]
-    poster = params[:poster]
-    save_path = Rails.root.join("public/audio/#{audio.original_filename}")
-    audio.rewind
-      # Open and write the file to file system.
-      File.open(save_path, 'wb') do |f|
-        f.write audio.read
-      end
-    render :text=> 'hi'
-    c = Comment.new
-    c.name = poster
-    c.clip = "#{audio.original_filename}"
-    c.score = 0
-    c.save!
+		audio = params[:audio]
+		poster = params[:poster]
+		save_path = Rails.root.join("public/audio/#{audio.original_filename}")
+		audio.rewind
+	  	# Open and write the file to file system.
+			File.open(save_path, 'wb') do |f|
+				f.write audio.read
+	    end
+		render :text=> 'hi'
+		c = Comment.new
+		c.name = poster
+		c.clip = "#{audio.original_filename}"
+		c.score = 0
+		c.save!
 	end
 
 	def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ActiveRecord::Base
 
   attr_accessible :clip, :name, :score
 
-  # attr_accessor :name
-
   mount_uploader :Clip, ClipUploader
 
   validates :name, presence: true
